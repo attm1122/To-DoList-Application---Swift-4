983aab3c613b7eccb0d70db75badb76c83d15a2f
Aubrey Mazinyi <attm1122@gmail.com>
2018-02-27 21:24:01 +1100

    Initial Commit

---
+++ To DoList Application - Swift 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To DoList Application - Swift 4
+//
+//  Created by Aubrey Mazinyi on 27/2/18.
+//  Copyright © 2018 AubijuanApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
